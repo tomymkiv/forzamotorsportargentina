95b9706cd97589a7e6f99ddddc8fc5fd9e147070
Tomas <liberfran.04@gmail.com>
2025-08-09 13:40:45 -0300

    Intento de deploy con React Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,31 +1,23 @@
-// import { StrictMode } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { HashRouter, Route, Routes } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import NotFoundPage from './NotFoundPage.tsx'
 import Categorias from './Categorias.tsx'
 import Carreras from './Carreras.tsx'
 
-// createBrowserRouter,
 // Creador de rutas
-// const router = createBrowserRouter([
-//   { path: '/', element: <App /> }, // asigno la ruta '/' al componente "app"
-//   { path: '/categorias', element: <Categorias /> },
-//   { path: '/carreras', element: <Carreras /> },
-//   { path: '*', element: <NotFoundPage /> },
-// ])
+const router = createBrowserRouter([
+  { path: '/', element: <App /> }, // asigno la ruta '/' al componente "app"
+  { path: '/categorias', element: <Categorias /> },
+  { path: '/carreras', element: <Carreras /> },
+  { path: '*', element: <NotFoundPage /> },
+])
 
 createRoot(document.getElementById('root')!).render(
-  <HashRouter>
-    <Routes>
-      <Route path='/' element={<App />} />
-      <Route path='/categorias' element={<Categorias />} />
-      <Route path='/carreras' element={<Carreras />} />
-      <Route path='*' element={<NotFoundPage />} />
-    </Routes>
-  </HashRouter>
-  // <StrictMode>
-  //   <RouterProvider router={router} />
-  // </StrictMode>
+  <StrictMode>
+    {/* la ruta es la constante declarada mas arriba */}
+    <RouterProvider router={router} />
+  </StrictMode>,
 )
